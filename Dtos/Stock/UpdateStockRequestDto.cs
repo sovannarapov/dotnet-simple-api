50c938ebc80c46ac54085158a9012dc4e83ad5d7
sovannarapov <sovannarapov9@gmail.com>
2025-01-19 16:36:10 +0700

    refactor: improve init value

--- Dtos/Stock/UpdateStockRequestDto.cs
+++ Dtos/Stock/UpdateStockRequestDto.cs
@@ -2,11 +2,11 @@ namespace api.Dtos.Stock
 {
     public class UpdateStockRequestDto
     {
-        public string Symbol { get; set; } = "";
-        public string CompanyName { get; set; } = "";
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
         public decimal Purchase { get; set; }
         public decimal LastDiv { get; set; }
-        public string Industry { get; set; } = "";
+        public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
     }
 }
