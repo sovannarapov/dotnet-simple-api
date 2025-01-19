c20d5ed0fd4f90bda3ac6cbd1e9cdb7a30590848
sovannarapov <sovannarapov9@gmail.com>
2025-01-19 16:26:56 +0700

    refactor: format code

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.Models
@@ -6,13 +7,22 @@ namespace api.Models
     public class Stock
     {
         public int Id { get; set; }
+        
+        [StringLength(120)]
         public string Symbol { get; set; } = "";
+        
+        [StringLength(120)]
         public string CompanyName { get; set; } = "";
+        
         [Column(TypeName = "decimal(18,2)")]
         public decimal Purchase { get; set; }
+        
         [Column(TypeName = "decimal(18,2)")]
         public decimal LastDiv { get; set; }
+        
+        [StringLength(120)]
         public string Industry { get; set; } = "";
+        
         public long MarketCap { get; set; }
 
         public List<Comment> Comments { get; set; } = new List<Comment>();
