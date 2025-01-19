e52d0a68023b8c923864083111d0dfb2b38aba63
sovannarapov <sovannarapov9@gmail.com>
2025-01-19 16:38:47 +0700

    refactor: improve naming param and param type

--- Interfaces/ICommentRepository.cs
+++ Interfaces/ICommentRepository.cs
@@ -7,8 +7,8 @@ namespace api.Interfaces
     {
         Task<List<Comment>> GetAllAsync();
         Task<Comment?> GetByIdAsync(int id);
-        Task<Comment> CreateAsync(Comment commentModel);
-        Task<Comment?> UpdateAsync(int id, UpdateCommentRequestDto commentDto);
+        Task<Comment> CreateAsync(Comment comment);
+        Task<Comment?> UpdateAsync(int id, Comment comment);
         Task<Comment?> DeleteAsync(int id);
     }
 }
