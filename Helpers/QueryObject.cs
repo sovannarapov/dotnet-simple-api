4e37424fcd0891a2f0734d75e090648bf19c0df5
sovannarapov <sovannarapov9@gmail.com>
2025-01-19 18:32:41 +0700

    feat: add QueryObject

---
+++ Helpers/QueryObject.cs
@@ -0,0 +1,9 @@
+namespace api.Helpers
+{
+    public class QueryObject
+    {
+        public string? CompanyName { get; set; } = null;
+        
+        public string? Industry { get; set; } = null;
+    }    
+}
