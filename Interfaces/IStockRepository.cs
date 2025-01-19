1802c3f91599d41ffce9cbdbfbe2a7bac3e7b899
sovannarapov <sovannarapov9@gmail.com>
2025-01-19 16:37:31 +0700

    refactor: improve naming param

--- Interfaces/IStockRepository.cs
+++ Interfaces/IStockRepository.cs
@@ -7,8 +7,8 @@ namespace api.Interfaces
     {
         Task<List<Stock>> GetAllAsync();
         Task<Stock?> GetByIdAsync(int id);
-        Task<Stock> CreateAsync(Stock stockModel);
-        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock> CreateAsync(Stock stock);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto updateStockRequestDto);
         Task<Stock?> DeleteAsync(int id);
         Task<bool> StockExists(int id);
     }
