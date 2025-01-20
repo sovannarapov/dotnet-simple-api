7b314d628193fb2780481dac85c444b0f4b73e11
sovannarapov <sovannarapov9@gmail.com>
2025-01-20 21:25:49 +0700

    feat: add login method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace api.Controllers
@@ -13,11 +14,35 @@ namespace api.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signInManager;
         
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(user =>
+                user.Email == loginRequestDto.Email.ToLower());
+
+            if (user == null) return Unauthorized("Invalid email!");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginRequestDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized("Email is not found or incorrect password");
+
+            return Ok(new NewUserDto
+            {
+                Username = user.UserName,
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)
+            });
         }
 
         [HttpPost("register")]
