e20b5083eef60f58865496aabd0da0322484f64f
sovannarapov <sovannarapov9@gmail.com>
2025-01-20 16:36:42 +0700

    feat: add Account to Dtos

---
+++ Dtos/Account/RegisterAccountRequestDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos.Account
+{
+    public class RegisterAccountRequestDto
+    {
+        [Required]
+        public string? Username { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        
+        [Required]
+        public string? Password { get; set; }
+    }
+}
