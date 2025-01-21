20a2d17df78fe357149ae708e197e84bc7fc7cfe
sovannarapov <sovannarapov9@gmail.com>
2025-01-21 20:59:02 +0700

    refactor: clean up

--- Models/Portfolio.cs
+++ Models/Portfolio.cs
@@ -6,11 +6,8 @@ namespace api.Models
     public class Portfolio
     {
         public string AppUserId { get; set; }
-        
         public int StockId { get; set; }
-
         public AppUser AppUser { get; set; }
-
         public Stock Stock { get; set; }
     }
 }
