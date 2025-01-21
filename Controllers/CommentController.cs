15abf066a7ef9cba91e42848baee2d800cb4de12
sovannarapov <sovannarapov9@gmail.com>
2025-01-21 21:15:43 +0700

    refactor: improve create method

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,6 +1,9 @@
 using api.Dtos.Comment;
+using api.Extensions;
 using api.Interfaces;
 using api.Mappers;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -11,11 +14,13 @@ namespace api.Controllers
     {
         private readonly ICommentRepository _commentRepository;
         private readonly IStockRepository _stockRepository;
+        private readonly UserManager<AppUser> _userManager;
 
-        public CommentController(ICommentRepository commentRepository, IStockRepository stockRepository)
+        public CommentController(ICommentRepository commentRepository, IStockRepository stockRepository, UserManager<AppUser> userManager)
         {
             _commentRepository = commentRepository;
             _stockRepository = stockRepository;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -52,8 +57,12 @@ namespace api.Controllers
                 return BadRequest("Stock does not exist");
             }
 
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+            
             var comment = createCommentRequestDto.ToCommentFromCreateDto(stockId);
 
+            comment.AppUserId = appUser.Id;
             await _commentRepository.CreateAsync(comment);
 
             return CreatedAtAction(nameof(GetById), new { Id = comment.Id }, comment.ToCommentDto());
