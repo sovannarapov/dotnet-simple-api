937041f52bff874fd5d3e8b41d2c613c137f9198
sovannarapov <sovannarapov9@gmail.com>
2025-01-30 18:33:55 +0700

    refactor: update controller to use service

---
+++ src/Core/Interfaces/ICommentService.cs
@@ -0,0 +1,12 @@
+using api.Core.Entities;
+
+namespace api.Core.Interfaces;
+
+public interface ICommentService
+{
+    Task<List<Comment>> GetAllAsync();
+    Task<Comment?> GetByIdAsync(int id);
+    Task<Comment> CreateAsync(Comment comment);
+    Task<Comment?> UpdateAsync(int id, Comment comment);
+    Task<Comment?> DeleteAsync(int id);
+}
