6da630315eb5d5c4409901c26b5751f6de1f7e53
sovannarapov <sovannarapov9@gmail.com>
2025-01-31 00:39:18 +0700

    refactor: clean up

--- src/Presentation/Controllers/StockController.cs
+++ src/Presentation/Controllers/StockController.cs
@@ -14,19 +14,15 @@ namespace api.Presentation.Controllers;
 public class StockController : ControllerBase
 {
     private readonly IStockService _stockService;
-    private readonly ILogger<StockController> _logger;
     
-    public StockController(IStockService stockService, ILogger<StockController> logger)
+    public StockController(IStockService stockService)
     {
         _stockService = stockService;
-        _logger = logger;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll([FromQuery] QueryObject queryObject)
     {
-        _logger.LogInformation("Getting all stocks");
-        
         var stocks = await _stockService.GetAllAsync(queryObject);
         var stockDto = stocks.Select(s => s.ToStockDto()).ToList();
 
@@ -36,8 +32,6 @@ public class StockController : ControllerBase
     [HttpGet("{id:int}")]
     public async Task<IActionResult> GetById([FromRoute] int id)
     {
-        _logger.LogInformation("Getting stock by id");
-        
         var stock = await _stockService.GetByIdAsync(id);
 
         if (stock == null)
@@ -51,15 +45,11 @@ public class StockController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
     {
-        _logger.LogInformation("Creating stock");
-        
         if (!ModelState.IsValid) return BadRequest(ModelState);
         
         var stock = stockDto.ToStockFromCreateDto();
 
         await _stockService.CreateAsync(stock);
-        
-        _logger.LogInformation("Creating stock successful");
         
         return CreatedAtAction(nameof(GetById), new { Id = stock.Id }, stock.ToStockWithoutCommentsDto());
     }
@@ -67,8 +57,6 @@ public class StockController : ControllerBase
     [HttpPut("{id:int}")]
     public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
     {
-        _logger.LogInformation("Updating stock by id {id}", id);
-        
         if (!ModelState.IsValid) return BadRequest(ModelState);
         
         var stock = await _stockService.UpdateAsync(id, updateDto);
@@ -77,8 +65,6 @@ public class StockController : ControllerBase
         {
             return NotFound();
         }
-        
-        _logger.LogInformation("Updating stock successful");
 
         return Ok(stock.ToStockWithoutCommentsDto());
     }
@@ -86,16 +72,12 @@ public class StockController : ControllerBase
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> Delete([FromRoute] int id)
     {
-        _logger.LogInformation("Deleting stock by id {id}", id);
-        
         var stockModel = await _stockService.DeleteAsync(id);
 
         if (stockModel == null)
         {
             return NotFound();
         }
-        
-        _logger.LogInformation("Deleting stock successful");
 
         return NoContent();
     }
