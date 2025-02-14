c68f8cf52b332d4a947cc85d0cf9df573664d4b1
sovannarapov <sovannarapov9@gmail.com>
2025-02-14 22:55:48 +0700

    refactor: update StockRepositoryTests

--- Tests/Infrastructure/Data/StockRepositoryTests.cs
+++ Tests/Infrastructure/Data/StockRepositoryTests.cs
@@ -8,29 +8,7 @@ namespace api.Tests.Infrastructure.Data;
 
 public class StockRepositoryTests
 {
-    private static async Task<ApplicationDbContext> GetDatabaseContext()
-    {
-        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseSqlServer(
-                "Data Source=localhost;Initial Catalog=pj_simple_api;User Id=sa;Password=P@ssw0rd;Integrated Security=True;TrustServerCertificate=true;Trusted_Connection=false")
-            .Options;
-
-        var context = new ApplicationDbContext(options);
-
-        await context.Database.EnsureCreatedAsync();
-
-        if (await context.Users.AnyAsync()) return context;
-
-        context.Users.Add(new AppUser
-        {
-            Email = "test@gmail.com",
-            UserName = "test"
-        });
-
-        await context.SaveChangesAsync();
-
-        return context;
-    }
+    private static Task<ApplicationDbContext> GetDatabaseContext() => TestDatabaseHelper.GetDatabaseContext();
 
     [Fact]
     public async Task AddStock_ShouldAddStock()
