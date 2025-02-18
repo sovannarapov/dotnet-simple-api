a88859ba45113486a422459b4eb83f15c78f4d0b
sovannarapov <sovannarapov9@gmail.com>
2025-02-18 18:07:29 +0700

    refactor: update ClaimExtensions

--- src/Common/Extensions/ClaimExtensions.cs
+++ src/Common/Extensions/ClaimExtensions.cs
@@ -4,12 +4,11 @@ namespace api.Common.Extensions;
 
 public static class ClaimExtensions
 {
-    public static string GetUsername(this ClaimsPrincipal user)
+    public static string? GetUsername(this ClaimsPrincipal? user)
     {
-        return user.Claims
-            .SingleOrDefault(
+        return user?.Claims.SingleOrDefault(
                 claim => claim.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname")
             )
-            .Value;
+            ?.Value;
     }
 }
