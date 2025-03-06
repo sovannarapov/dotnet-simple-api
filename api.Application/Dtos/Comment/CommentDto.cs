53bf769de65a600ab19582e5c9d9f0ec57ed1721
sovannarapov <sovannarapov9@gmail.com>
2025-03-06 13:37:11 +0700

    refactor: update CommentDto

--- api.Application/Dtos/Comment/CommentDto.cs
+++ api.Application/Dtos/Comment/CommentDto.cs
@@ -6,6 +6,6 @@ public class CommentDto
     public string Title { get; set; } = "";
     public string Content { get; set; } = "";
     public DateTime CreatedOn { get; set; } = DateTime.Now;
-    public string CreatedBy { get; set; } = string.Empty;
+    public string? CreatedBy { get; set; } = string.Empty;
     public int? StockId { get; set; }
 }
