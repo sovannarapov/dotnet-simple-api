32bfca35f27a26b3cf3383083b3cdf2426eb60b4
sovannarapov <sovannarapov9@gmail.com>
2025-03-07 17:01:08 +0700

    refactor: update create and delete command handler

--- api.Application/Features/Portfolios/Commands/CreatePortfolio/CreatePortfolioCommandHandler.cs
+++ api.Application/Features/Portfolios/Commands/CreatePortfolio/CreatePortfolioCommandHandler.cs
@@ -1,6 +1,6 @@
 using api.Application.Interfaces;
 using api.Core.Entities;
-using api.Core.Interfaces;
+using api.Core.Interfaces.IPortfolio;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -9,18 +9,21 @@ namespace api.Application.Features.Portfolios.Commands.CreatePortfolio;
 
 public class CreatePortfolioCommandHandler : IRequestHandler<CreatePortfolioCommand, Portfolio>
 {
-    private readonly IPortfolioRepository _portfolioRepository;
+    private readonly IPortfolioWriteRepository _portfolioRepository;
+    private readonly IPortfolioReadRepository _portfolioReadRepository;
     private readonly IStockService _stockService;
     private readonly UserManager<AppUser> _userManager;
 
     public CreatePortfolioCommandHandler(
         IStockService stockService,
-        IPortfolioRepository portfolioRepository,
+        IPortfolioWriteRepository portfolioRepository,
+        IPortfolioReadRepository portfolioReadRepository,
         UserManager<AppUser> userManager
     )
     {
         _stockService = stockService;
         _portfolioRepository = portfolioRepository;
+        _portfolioReadRepository = portfolioReadRepository;
         _userManager = userManager;
     }
 
@@ -29,7 +32,7 @@ public class CreatePortfolioCommandHandler : IRequestHandler<CreatePortfolioComm
         var appUser = await _userManager.FindByNameAsync(request.Username) ?? throw new BadHttpRequestException("User does not exist");
         var stock = await _stockService.GetBySymbolAsync(request.Symbol) ?? throw new KeyNotFoundException("Stock not found");
 
-        var userPortfolio = await _portfolioRepository.GetUserPortfolio(appUser);
+        var userPortfolio = await _portfolioReadRepository.GetUserPortfolio(appUser);
 
         if (userPortfolio.Any(s => string.Equals(s.Symbol.ToLower(), request.Symbol.ToLower())))
             throw new BadHttpRequestException("Stock already in portfolio");
--- api.Application/Features/Portfolios/Commands/DeletePortfolio/DeletePortfolioCommandHandler.cs
+++ api.Application/Features/Portfolios/Commands/DeletePortfolio/DeletePortfolioCommandHandler.cs
@@ -1,5 +1,5 @@
 using api.Core.Entities;
-using api.Core.Interfaces;
+using api.Core.Interfaces.IPortfolio;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 
@@ -7,19 +7,24 @@ namespace api.Application.Features.Portfolios.Commands.DeletePortfolio;
 
 public class DeletePortfolioCommandHandler : IRequestHandler<DeletePortfolioCommand, string>
 {
-    private readonly IPortfolioRepository _portfolioRepository;
+    private readonly IPortfolioWriteRepository _portfolioRepository;
+    private readonly IPortfolioReadRepository _portfolioReadRepository;
     private readonly UserManager<AppUser> _userManager;
 
-    public DeletePortfolioCommandHandler(IPortfolioRepository portfolioRepository, UserManager<AppUser> userManager)
+    public DeletePortfolioCommandHandler(
+        IPortfolioWriteRepository portfolioRepository,
+        UserManager<AppUser> userManager,
+        IPortfolioReadRepository portfolioReadRepository)
     {
         _portfolioRepository = portfolioRepository;
+        _portfolioReadRepository = portfolioReadRepository;
         _userManager = userManager;
     }
 
     public async Task<string> Handle(DeletePortfolioCommand request, CancellationToken cancellationToken)
     {
         var appUser = await _userManager.FindByNameAsync(request.Username) ?? throw new KeyNotFoundException("User not found");
-        var userPortfolio = await _portfolioRepository.GetUserPortfolio(appUser);
+        var userPortfolio = await _portfolioReadRepository.GetUserPortfolio(appUser);
 
         var filteredStock = userPortfolio.Where(stock => string.Equals(stock.Symbol.ToLower(), request.Symbol.ToLower())).ToList();
 
