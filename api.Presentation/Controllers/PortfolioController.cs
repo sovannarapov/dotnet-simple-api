6247542c758df69d41f2238238c30d27ccd0c92b
sovannarapov <sovannarapov9@gmail.com>
2025-03-07 16:35:20 +0700

    refactor: update PortfolioController

--- api.Presentation/Controllers/PortfolioController.cs
+++ api.Presentation/Controllers/PortfolioController.cs
@@ -1,7 +1,10 @@
-using api.Application.Interfaces;
+using api.Application.Features.Portfolios.Commands.CreatePortfolio;
+using api.Application.Features.Portfolios.Commands.DeletePortfolio;
+using api.Application.Features.Portfolios.Queries.GetPortfolio;
 using api.Common;
 using api.Common.Extensions;
 using api.Core.Entities;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -13,79 +16,78 @@ namespace api.Presentation.Controllers;
 [ApiController]
 public class PortfolioController : ControllerBase
 {
-    private readonly IPortfolioService _portfolioService;
-    private readonly IStockService _stockService;
-    private readonly UserManager<AppUser> _userManager;
+    private readonly IMediator _mediator;
 
     public PortfolioController(
         UserManager<AppUser> userManager,
-        IStockService stockService,
-        IPortfolioService portfolioService
+        IMediator mediator
     )
     {
-        _userManager = userManager;
-        _stockService = stockService;
-        _portfolioService = portfolioService;
+        _mediator = mediator;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetUserPortfolio()
     {
-        var username = User.GetUsername();
-        var appUser = await _userManager.FindByNameAsync(username);
+        try
+        {
+            var username = User.GetUsername();
 
-        if (appUser == null) return NotFound("User not found");
+            if (username is null) return NotFound("User not found");
 
-        var userPortfolio = await _portfolioService.GetUserPortfolio(appUser);
-        return Ok(userPortfolio);
+            var command = new GetPortfolioQuery(username);
+
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            Console.WriteLine("Error >>> " + ex.Message);
+            return StatusCode(StatusCodes.Status404NotFound, ex.Message);
+        }
     }
 
     [HttpPost]
     public async Task<IActionResult> AddPortfolio(string symbol)
     {
-        var username = User.GetUsername();
-        var appUser = await _userManager.FindByNameAsync(username);
-        var stock = await _stockService.GetBySymbolAsync(symbol);
-
-        if (stock == null) return BadRequest("Stock not found");
-
-        var userPortfolio = await _portfolioService.GetUserPortfolio(appUser);
-
-        if (userPortfolio.Any(e => e.Symbol.ToLower() == symbol.ToLower()))
-            return BadRequest("Stock already in portfolio");
-
-        var portfolio = new Portfolio
+        try
         {
-            AppUserId = appUser.Id,
-            StockId = stock.Id
-        };
+            var username = User.GetUsername();
 
-        await _portfolioService.CreateAsync(portfolio);
+            if (username is null) return NotFound("User not found");
 
-        if (portfolio == null) return BadRequest("Failed to add stock to portfolio");
+            var command = new CreatePortfolioCommand(symbol, username);
 
-        return Created("Portfolio created successfully", new
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+        catch (Exception ex)
         {
-            portfolio.AppUserId,
-            portfolio.StockId,
-            portfolio.Stock
-        });
+            Console.WriteLine("Error >>> " + ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
     }
 
     [HttpDelete]
     public async Task<IActionResult> DeletePortfolio(string symbol)
     {
-        var username = User.GetUsername();
-        var appUser = await _userManager.FindByNameAsync(username);
-        var userPortfolio = await _portfolioService.GetUserPortfolio(appUser);
+        try {
+            var username = User.GetUsername();
 
-        var filteredStock = userPortfolio.Where(stock => stock.Symbol.ToLower() == symbol.ToLower()).ToList();
+            if (username is null) return NotFound("User not found");
 
-        if (filteredStock.Count == 1)
-            await _portfolioService.DeleteAsync(appUser, symbol);
-        else
-            return BadRequest("Stock not found in portfolio");
+            var command = new DeletePortfolioCommand(symbol, username);
 
-        return Ok();
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error >>> " + ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
     }
 }
